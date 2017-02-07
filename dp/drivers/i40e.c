77e4a41e2be6f0601bbb15ac18e2737fc70c6f55
Andy Roulin <andy.roulin@epfl.ch>
2017-02-07 16:27:26 -0500

    Don't use flow director for TX

--- dp/drivers/i40e.c
+++ dp/drivers/i40e.c
@@ -283,7 +283,6 @@ static int dev_start(struct ix_rte_eth_dev *dev)
 		#ifdef RTE_LIBRTE_IEEE1588
 			tx_ctx.timesync_ena = 1;
 		#endif
-		tx_ctx.fd_ena = TRUE;
 		tx_ctx.base = txq->ring_physaddr / I40E_QUEUE_BASE_ADDR_UNIT;
 		tx_ctx.qlen = txq->len;
 
